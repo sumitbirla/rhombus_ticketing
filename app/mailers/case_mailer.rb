04ee2fb4baab5195e2a5ddf41389efc036b55fed
Sumit Birla <sbirla@tampahost.net>
2015-03-19 01:01:35 -0400

    mailer fix

--- app/mailers/case_mailer.rb
+++ app/mailers/case_mailer.rb
@@ -34,17 +34,17 @@ class CaseMailer < ApplicationMailer
          )
   end
   
-  def updated(c)
-    @case = c
-    headers["X-Rhombus-Case-ID"] = @case.id
+  def updated(case_update)
+    @case_update = case_update
+    headers["X-Rhombus-Case-ID"] = case_update.case.id
   
     options = {
         address: Cache.setting(Rails.configuration.domain_id, :system, 'SMTP Server'),
         openssl_verify_mode: 'none'
     }
-    mail(from: "#{@case.case_queue.reply_name} <#{@case.case_queue.reply_email}>",
-         to: User.find(@case.assigned_to).email,
-         subject: "Case ##{@case.id} has an update",
+    mail(from: "#{case_update.case.case_queue.reply_name} <#{case_update.case.case_queue.reply_email}>",
+         to: User.find(case_update.case.assigned_to).email,
+         subject: "Case ##{case_update.case.id} has an update",
          delivery_method_options: options
          )
   end
