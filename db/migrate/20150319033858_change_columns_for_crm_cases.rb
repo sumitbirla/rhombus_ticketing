220ce1b4091025c39a8e05c1152a67a6f4de79e1
Sumit Birla <sbirla@tampahost.net>
2015-03-28 20:19:32 -0400

    migration fix

--- db/migrate/20150319033858_change_columns_for_crm_cases.rb
+++ db/migrate/20150319033858_change_columns_for_crm_cases.rb
@@ -1,8 +1,11 @@
 class ChangeColumnsForCrmCases < ActiveRecord::Migration
   def change
-	change_column :crm_cases, :attachments, :string
-	rename_column :crm_case_updates, :data, :raw_data
-	add_column :crm_case_updates, :attachments, :string
-	add_column :crm_case_updates, :received_at, :datetime, null: false
+  	change_column :crm_cases, :attachments, :string
+  	rename_column :crm_case_updates, :data, :raw_data
+  	add_column :crm_case_updates, :attachments, :string
+  	add_column :crm_case_updates, :received_at, :datetime, null: false
+    add_column :crm_case_updates, :new_state, :string
+    add_column :crm_case_updates, :new_priority, :string
+    add_column :crm_case_updates, :new_assignee, :integer
   end
 end
