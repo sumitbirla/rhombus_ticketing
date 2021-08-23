e4352e149f979f207ffc598c7107d7cd0dea161a
Sumit Birla <sbirla@tampahost.net>
2021-08-23 08:46:49 -0400

    User-defined ticket priority colors

--- app/helpers/case_helper.rb
+++ app/helpers/case_helper.rb
@@ -1,7 +1,5 @@
 module CaseHelper
-
   def case_status_indicator(status)
-
     status = status.status if status.class.name == "Case"
 
     if status == 'new'
@@ -16,20 +14,29 @@ module CaseHelper
   end
 
   def case_priority_indicator(priority)
-
     priority = priority.priority if priority.class.name == "Case"
 
-    if priority == 'high'
-      css_class = 'label-warning'
-    elsif priority == 'normal'
-      css_class = 'label-default'
-    elsif priority == 'urgent'
-      css_class = 'label-danger'
+    list_item = ListItem.joins(:list)
+                        .where(value: priority)
+                        .where("cms_lists.name = ? AND cms_lists.affiliate_id = ?", :ticket_priorities, current_user.affiliate_id)
+                        .first
+
+    if list_item.nil?
+      if priority == 'high'
+        css_class = 'label-warning'
+      elsif priority == 'normal'
+        css_class = 'label-default'
+      elsif priority == 'urgent'
+        css_class = 'label-danger'
+      else
+        css_class = 'label-default'
+      end
+
+      str = "<span class='label #{css_class}'>#{priority}</span>"
     else
-      css_class = 'label-default'
+      str = "<span class='label' style='background-color: #{list_item.background_color}; color: #{list_item.foreground_color}'>#{priority}</span>"
     end
 
-    "<span class='label #{css_class}'>#{priority}</span>".html_safe
+    str.html_safe
   end
-
 end
